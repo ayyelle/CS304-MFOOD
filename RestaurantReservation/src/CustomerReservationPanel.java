59ca590812ac02f456efcf7bae8b8d5bd957eb05
Lauren <lauren.e.coombe@gmail.com>
2016-06-04 18:18:56 -0700

    Added front end for Customer reservation requests

--- RestaurantReservation/src/CustomerReservationPanel.java
+++ RestaurantReservation/src/CustomerReservationPanel.java
@@ -17,6 +17,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
@@ -34,69 +35,78 @@ public class CustomerReservationPanel extends JPanel {
 	String customerId;
 	JLabel title;
 	SQLRestaurant s;
+	Vector<String> restaurantOptions;
+	JComboBox restaurantComboBox;
+	JPanel formPanel;
+	GridBagLayout layout;
+	GridBagConstraints gc;
+	JLabel restaurantLabel;
+	JLabel dateLabel;
+	JButton submit;
+	JComboBox timesComboBox;
+	Vector<String> timeOptions;
+	
+	JLabel timeLabel;
+	
+	UtilDateModel model;
+	Properties p;
+	
 
 	public CustomerReservationPanel(CustomerPanel parent) {
 		this.parent = parent;
 		
 		this.setLayout(new BorderLayout());
+		initializeElements();
 		
-		s = new SQLRestaurant();
-		JPanel formPanel = new JPanel();
-		
-		title = new JLabel("Make a reservation!");
-		title.setFont(new Font(title.getName(), Font.PLAIN, 30));
 		this.add(title, BorderLayout.NORTH);
-		formPanel.setPreferredSize(new Dimension(10, 10));
 		this.add(formPanel, BorderLayout.CENTER);
-		formPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
-				GridBagLayout layout = new GridBagLayout();
-		GridBagConstraints gc = new GridBagConstraints();
 		formPanel.setLayout(layout);
-		JLabel label = new JLabel("Choose a restaurant: ");
 		gc.insets = new Insets(10, 10, 10, 10);
 		gc.gridx = 0;
 		gc.gridy = 0;
-		formPanel.add(label, gc);
-		
-		s = new SQLRestaurant();
-		
-		Vector<String> restaurantOptions = s.getRestaurants();
-		JComboBox restaurantCombo = new JComboBox(restaurantOptions);
+		formPanel.add(restaurantLabel, gc);
+				
 		gc.gridx = 1;
-		formPanel.add(restaurantCombo, gc);
+		formPanel.add(restaurantComboBox, gc);
 		
-
-		JLabel label1 = new JLabel("Choose Date: ");
 		gc.gridx = 0;
 		gc.gridy = 1;
-		formPanel.add(label1, gc);
-		
-		UtilDateModel model = new UtilDateModel();
-		Properties p = new Properties();
-		p.put("text.today", "Today");
-		p.put("text.month", "Month");
-		p.put("text.month", "Month");
-		p.put("text.year", "Year");
+		formPanel.add(dateLabel, gc);
+
 		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
 		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateComponentFormatter());
-		JTextField text1 = new JTextField("Text field 2", 15);
+
 		gc.gridx = 1;
 		formPanel.add(datePicker, gc);
 		
-		JButton submit = new JButton("submit");
-		gc.gridx = 0;
 		gc.gridy = 2;
+		gc.gridx = 0;
+		formPanel.add(timeLabel, gc);
+		
+		gc.gridx = 1;
+		formPanel.add(timesComboBox, gc);
+		
+		gc.gridx = 1;
+		gc.gridy = 3;
 		formPanel.add(submit, gc);
 		
+		//Getting the information after submit button pressed
 		submit.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				Date selectedDate = (Date) datePicker.getModel().getValue();
-				System.out.println("Day: " + selectedDate.getDate());
-				System.out.println("Month: " + (selectedDate.getMonth()+1));
-				System.out.println("Year: " + (selectedDate.getYear()+1900));
+				String selectedTime = (String) timesComboBox.getSelectedItem();
+				if (selectedDate == null) {
+					JOptionPane.showMessageDialog(null,"Please select a date!", "Date Missing", JOptionPane.PLAIN_MESSAGE);
+
+				} else {
+					System.out.println("Day: " + selectedDate.getDate());
+					System.out.println("Month: " + (selectedDate.getMonth()+1));
+					System.out.println("Year: " + (selectedDate.getYear()+1900));
+					System.out.println("Time: " + selectedTime);
+				}
 				
 			}
 			
@@ -113,6 +123,48 @@ public class CustomerReservationPanel extends JPanel {
 	}
 
 
+
+	private void initializeElements() {
+		s = new SQLRestaurant();
+		
+		formPanel = new JPanel();
+		formPanel.setPreferredSize(new Dimension(10, 10));
+		formPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+		
+		title = new JLabel("Make a reservation!");
+		title.setFont(new Font(title.getName(), Font.PLAIN, 30));
+		
+		layout = new GridBagLayout();
+		gc = new GridBagConstraints();
+		 
+		restaurantOptions = s.getRestaurants();
+		restaurantComboBox = new JComboBox(restaurantOptions);
+
+		restaurantLabel = new JLabel("Choose a restaurant: ");
+
+		dateLabel = new JLabel("Choose Date: ");
+
+		//Date Picker stuff
+		model = new UtilDateModel();
+		p = new Properties();
+		p.put("text.today", "Today");
+		p.put("text.month", "Month");
+		p.put("text.month", "Month");
+		p.put("text.year", "Year");
+
+		submit = new JButton("Submit Reservation Request");
+		
+		timeLabel = new JLabel("Select a reservation time:");
+		timeOptions = new Vector<String>();
+		for (int i = 5; i <= 11; i++) {
+			String str = i + ":00 PM";
+			timeOptions.add(str);
+		}
+		timesComboBox = new JComboBox(timeOptions);
+		
+		
+	}
 
 
 
