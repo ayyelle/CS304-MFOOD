fb3ea4d1ec1a77a934c7a839a8a294a363110fe2
Lauren <lauren.e.coombe@gmail.com>
2016-06-09 21:46:52 -0700

    Added fix for reservations appearing in drop-down box on deletions page

--- RestaurantReservation/src/CustomerDeleteReservation.java
+++ RestaurantReservation/src/CustomerDeleteReservation.java
@@ -30,114 +30,121 @@ public class CustomerDeleteReservation extends JPanel {
 	JTable displayResult;
 	SQLRestaurant s;
 	SQLCustomer sc;
-	
+	Vector<String> tableBookings;
 
-	
+
+
 	public CustomerDeleteReservation(CustomerPanel parent) {
 		this.parent = parent;
 		s = new SQLRestaurant();
 		sc= new SQLCustomer();
 		//this.customerId = parent.getCustomerID();
-		
-	Vector<String> tableBookings = sc.getReservationsAsString(customerId);
-	this.setLayout(new GridBagLayout());
-	GridBagConstraints c = new GridBagConstraints();
-	title = new JLabel("See Menu", JLabel.CENTER);
-	title.setFont(new Font(title.getName(), Font.PLAIN, 20));
-	restaurantComboBox = new JComboBox(tableBookings);
-	restaurantLabel = new JLabel("Select a booking: ", JLabel.TRAILING);
 
-	displayResult = new JTable();
-	
-	displayResultPanel = new JScrollPane();
-	displayResultPanel.setPreferredSize(new Dimension(300, 300));
+		tableBookings = sc.getReservationsAsString(customerId);
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		title = new JLabel("See Menu", JLabel.CENTER);
+		title.setFont(new Font(title.getName(), Font.PLAIN, 20));
+		restaurantComboBox = new JComboBox();
+		restaurantLabel = new JLabel("Select a booking: ", JLabel.TRAILING);
 
-	submit = new JButton("Submit");
-	
-	submit.addActionListener(new ActionListener() {
+		displayResult = new JTable();
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			String booking = (String) restaurantComboBox.getSelectedItem();
-			String restaurantName = booking.substring(0, booking.indexOf("-") + 1);
-			System.out.println("restaurant name in delete " + restaurantName);
-			String startDayTime = booking.substring(booking.indexOf("-") + 1, booking.indexOf("-")+4);
-			System.out.println(startDayTime);
-			String partySize = booking.substring(booking.indexOf("-") + 5,booking.indexOf("-")+6);
-			System.out.println(partySize);
-			String tid = booking.substring(booking.indexOf("-") + 6);
-			System.out.println(tid);
+		displayResultPanel = new JScrollPane();
+		displayResultPanel.setPreferredSize(new Dimension(300, 300));
 
-			System.out.println(booking);
-			//Vector<String> colNames = new Vector<String>();
-			//colNames.add("Food Name");
-			//colNames.add("Price");
+		submit = new JButton("Submit");
 
-			//SQLRestaurant s = new SQLRestaurant();
-			sc.deleteBooking(restaurantName,startDayTime,customerId,tid);
-			//Vector<Vector> data = s.getMenuItems(restaurant);
-			//displayResult = new JTable(data, colNames);
+		submit.addActionListener(new ActionListener() {
 
-			//displayResult.getColumnModel().getColumn(0).setMaxWidth(250);
-			//displayResult.getColumnModel().getColumn(1).setMaxWidth(50);
-			
-			//displayResult.setRowHeight(40);
-			//displayResultPanel.getViewport().add(displayResult);
-			//if (data.size() == 0) {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String booking = (String) restaurantComboBox.getSelectedItem();
+				String restaurantName = booking.substring(0, booking.indexOf("-") + 1);
+				System.out.println("restaurant name in delete " + restaurantName);
+				String startDayTime = booking.substring(booking.indexOf("-") + 1, booking.indexOf("-")+4);
+				System.out.println(startDayTime);
+				String partySize = booking.substring(booking.indexOf("-") + 5,booking.indexOf("-")+6);
+				System.out.println(partySize);
+				String tid = booking.substring(booking.indexOf("-") + 6);
+				System.out.println(tid);
+
+				System.out.println(booking);
+				//Vector<String> colNames = new Vector<String>();
+				//colNames.add("Food Name");
+				//colNames.add("Price");
+
+				//SQLRestaurant s = new SQLRestaurant();
+				sc.deleteBooking(restaurantName,startDayTime,customerId,tid);
+				//Vector<Vector> data = s.getMenuItems(restaurant);
+				//displayResult = new JTable(data, colNames);
+
+				//displayResult.getColumnModel().getColumn(0).setMaxWidth(250);
+				//displayResult.getColumnModel().getColumn(1).setMaxWidth(50);
+
+				//displayResult.setRowHeight(40);
+				//displayResultPanel.getViewport().add(displayResult);
+				//if (data.size() == 0) {
 				//JOptionPane.showMessageDialog(null, "There are no food items to view, try another restaurant!", "No Menu To Display", JOptionPane.PLAIN_MESSAGE);
-			//}
-			
-		}
-		
-	});
-	
-	
-	c.gridx = 0;
-	c.gridy = 0;
-	c.gridwidth = c.REMAINDER;
-	c.anchor = c.PAGE_START;
-	c.insets = new Insets(30, 5, 5, 5);
-	this.add(title, c);
-	
-	
-	c.gridx = 0;
-	c.gridy = 1;
-	c.insets = new Insets(0, 0, 0, 0);
-	
-	this.add(restaurantLabel, c);
-	
-	c.gridx = 0;
-	c.gridy = 2;
+				//}
 
-	this.add(restaurantComboBox, c);
-	
-	c.insets = new Insets(10, 10, 10, 10);
+			}
 
-	c.gridx = 1;
-	c.gridy = 3;
-	this.add(submit, c);
-	
-	c.gridx = 1;
-	c.gridy = 4;
-	this.add(displayResultPanel, c);
-    
-    addComponentListener(new ComponentAdapter() {
-        @Override
-        public void componentShown(ComponentEvent evt) {
-            start();
-        }
-    });
-    
+		});
+
+
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = c.REMAINDER;
+		c.anchor = c.PAGE_START;
+		c.insets = new Insets(30, 5, 5, 5);
+		this.add(title, c);
+
+
+		c.gridx = 0;
+		c.gridy = 1;
+		c.insets = new Insets(0, 0, 0, 0);
+
+		this.add(restaurantLabel, c);
+
+		c.gridx = 0;
+		c.gridy = 2;
+
+		this.add(restaurantComboBox, c);
+
+		c.insets = new Insets(10, 10, 10, 10);
+
+		c.gridx = 1;
+		c.gridy = 3;
+		this.add(submit, c);
+
+		c.gridx = 1;
+		c.gridy = 4;
+		this.add(displayResultPanel, c);
+
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentShown(ComponentEvent evt) {
+				start();
+			}
+		});
+
 	}
-	
+
 	public void start() {
 		this.customerId = parent.getCustomerID();
 		System.out.println(customerId);
 		displayResultPanel.getViewport().remove(displayResult);
+		
 		Vector<String> tableBookings = sc.getReservationsAsString(customerId);
-		restaurantComboBox = new JComboBox(tableBookings);
-		//Vector<String> tableBookings = sc.getReservationsAsString(customerId);
-		//restaurantComboBox = new JComboBox(tableBookings);
+		
+		//Adding the Customer's reservations to the drop-down box
+		restaurantComboBox.removeAllItems();
+		for (int i = 0; i<tableBookings.size(); i++) {
+			restaurantComboBox.addItem(tableBookings.get(i));
+
+		}
+
 	}
-    
+
 }
